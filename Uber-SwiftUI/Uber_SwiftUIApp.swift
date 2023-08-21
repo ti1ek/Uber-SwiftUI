e416c51176c5606c44c9d6fb2674cebd82263baa
ti1ek <tilek.koszhanov@mail.ru>
2023-08-21 21:45:10 +0500

    Initial Commit

---
+++ Uber-SwiftUI/Uber_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uber_SwiftUIApp.swift
+//  Uber-SwiftUI
+//
+//  Created by ti1ek on 21.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Uber_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
